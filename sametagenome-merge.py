6224f0fbe9b3f48b84c8fd0981e22b3d3ff3a079
Moreno <moreno.zolfo@gmail.com>
2014-08-08 16:16:21 +0200

    Aug1

--- sametagenome-merge.py
+++ sametagenome-merge.py
@@ -19,7 +19,9 @@ class bcolors:
 parser = argparse.ArgumentParser()
 parser.add_argument("folder", help="folder containing .txt sametagenome files")
 parser.add_argument("-d","--database", help="database", default="bsb.db")
-#parser.add_argument("-i","--index", help="name of the output bowtie2 index", default="bwindex")
+parser.add_argument("--meta", help="metadata file (CSV)")
+parser.add_argument("--idField", help="field containig the 'sampleID' value", default=0, type=int)
+
 args=parser.parse_args()
  
  
@@ -68,13 +70,15 @@ for file in os.listdir(args.folder):
 		genes =line.split()[2::] 
 		
 		if organism not in cel: cel[organism] = []
-		cel[organism].append((dict((x.split('::')[0],x.split('::')[1].upper()) for x in genes),sampleName))
+		cel[organism].append((dict((x.split('::')[0],(x.split('::')[1].upper(),x.split('::')[2])) for x in genes),sampleName))
 
 for bacterium,bactRecord in cel.items():
+	#bactRecord = ( {gene_allele: (sequence,accuracy) , ...} , sampleName ) 
+	
 	print '\r\n----------------------------------------------------------------------\r\n'+bacterium  
 	 
-	profil = open(args.folder+'/merged_'+bacterium+'.txt','w')
-	isolafil = open(args.folder+'/isolate_'+bacterium+'.txt','w') 
+	profil = open(args.folder+'/ST_'+bacterium+'.txt','w')
+	
  	
 	profil.write(bacterium+'\r\n')
 	
@@ -103,21 +107,21 @@ for bacterium,bactRecord in cel.items():
 	
 	
 	profil.write('ST\t'+'\t'.join([x for x in sorted(lastGenes.keys())] )+'\r\n')
-	print 'ST\t'+'\t'.join([x for x in sorted(lastGenes.keys())])+'\tHits\tNotes'
+	print 'ST\t'+'\t'.join([x for x in sorted(lastGenes.keys())])+'\tHits'
 	
 	
 	for bacteriumLine,sampleRecord in bactRecord:
 		 
 		#bacteriumLine is a dict. Key: genes, Values: sequences. No sequence = no news.
-
+		#>> {gene_allele: (sequence,accuracy) , ...} 
 		profileLine = {}
 		newAlleles = []
+		sum_of_accuracies = 0.0
 		
-		
-		for geneLabel,geneSeq in bacteriumLine.items():
+		for geneLabel,(geneSeq,geneAccur) in bacteriumLine.items():
 			
 			geneOrganism,geneName,geneAllele = geneLabel.split('_')
-			
+			sum_of_accuracies += float(geneAccur)
 			if geneSeq == '' or sequenceExists(bacterium,geneSeq):
 				# WE HAVE A DATABASE SEQUENCE
 				#print "WE HAVE A DATABASE SEQUENCE"
@@ -140,9 +144,10 @@ for bacterium,bactRecord in cel.items():
 				newSequences.append(SeqRecord(Seq(geneSeq,IUPAC.unambiguous_dna),id=geneNewLabel, description = ''))
  
 		
+		meanAccuracy = sum_of_accuracies / float(len(bacteriumLine))
 		if len(newAlleles) == 0:
 		
-		## Existant MLST profile -> Look for it (--> ISOLATES)
+		## Existent MLST profile -> Look for it (--> ISOLATES)
 		
 			#Tries to define an existing MLST profile with the alleles
 			
@@ -153,7 +158,8 @@ for bacterium,bactRecord in cel.items():
 				# TODO: to isolates and report as "non-new"
 				print "\t | Existing MLST Profile", tryDefine[0][0]
 				oldProfiles[tryDefine[0][0]][0]+=1
-				isolates.append((tryDefine[0][0],sampleRecord)) 
+				isolates.append((tryDefine[0][0],meanAccuracy,sampleRecord)) 
+				continue
 			
 			
 			foundExistant = 0
@@ -164,16 +170,16 @@ for bacterium,bactRecord in cel.items():
 					
 			if foundExistant:
 				encounteredProfiles[foundExistant][1] += 1
-				isolates.append((foundExistant,sampleRecord)) 
+				isolates.append((foundExistant,meanAccuracy,sampleRecord)) 
 			else:
 				lastProfile+=1
 				encounteredProfiles[lastProfile] = [profileLine,1,2]
-				isolates.append((lastProfile,sampleRecord)) 
+				isolates.append((lastProfile,meanAccuracy,sampleRecord)) 
 		else:
 			# THIS IS A NEW PROFILE
 			lastProfile+=1
 			encounteredProfiles[lastProfile] = [profileLine,1,1]
-			isolates.append((lastProfile,sampleRecord)) 
+			isolates.append((lastProfile,meanAccuracy,sampleRecord)) 
 		
 		
 			
@@ -186,7 +192,7 @@ for bacterium,bactRecord in cel.items():
 		#                   | 
 	#Old profiles
 	
-	SeqIO.write(newSequences,args.folder+'/sequences_'+bacterium+'fasta', "fasta")
+	SeqIO.write(newSequences,args.folder+'/sequences_'+bacterium+'.faa', "fasta")
 	
 	for profileCode,profileTrack in oldProfiles.items():
 		profil.write(str(profileCode)+'\t'+'\t'.join([str(v) for k,v in sorted(profileTrack[1].items())])+'\r\n')
@@ -201,22 +207,35 @@ for bacterium,bactRecord in cel.items():
 		print profileNumber + '\t' + '\t'.join([bcolors.OKBLUE+str(v[0])+bcolors.ENDC if v[1] == 1 else bcolors.OKGREEN+str(v[0])+bcolors.ENDC if v[1] == 2 else str(v[0]) for k,v in sorted(profile.items())]) + '\t' + str(hits)
 			
 		profil.write(str(profileID)+'\t'+'\t'.join([str(v[0]) for k,v in sorted(profile.items())])+'\n')
-	
-	
-	identifiers = {}
-	for line in open('hmp_meta2.txt'):
-		l = line.split('\t') 
-		identifiers[l[0]] = {'RANDSID' : l[1],'VISNO' : l[2],'GENDER_C' : l[3],'Specimen_Type' : l[4], 'STArea': l[5]}
-	
-	isolafil.write('ST\tSample\tRANDSID\tVISNO\tGENDER_C\tSpecimen_Type\tSTArea\n')
-	for profileST,sampleName in isolates:
-		if sampleName in identifiers:
-			isolafil.write(str(profileST)+'\t'+str(sampleName)+'\t'+str(identifiers[sampleName]['RANDSID'])+'\t'+str(identifiers[sampleName]['VISNO'])+'\t'+str(identifiers[sampleName]['GENDER_C'])+'\t'+str(identifiers[sampleName]['Specimen_Type'])+'\t'+str(identifiers[sampleName]['STArea']))
-		else:
-			print "SAMPLE ",sampleName,"Not in identifiers"
-			isolafil.write(str(profileST)+'\t'+str(sampleName))
-		
-	isolafil.close()
 	profil.close()
+	
+	if args.meta:
+		isolafil = open(args.folder+'/isolate_'+bacterium+'.txt','w') 
+		identifiers = {}
+		p1line = 0
+		for line in open(args.meta):
+			if not p1line: 
+				p1line=1
+				keys = [str(x).strip() for x in line.split('\t')]
+			else:
+				l = line.strip().split('\t')
+				identifiers[l[args.idField]] = dict((keys[i],l[i]) for i in range(0,len(keys)))
+ 
+		 
+		
+		isolafil.write('ST\tAccuracy\t'+'\t'.join(keys)+'\n')
+		
+		for profileST,meanAccur,sampleName in isolates:
+			if sampleName in identifiers:
+				strl=[]
+				for ky in keys:
+					strl.append(identifiers[sampleName][ky])
+				isolafil.write(str(profileST)+'\t'+str(round(meanAccur,2))+'\t'+'\t'.join(strl)+'\n')
+			else:
+				print "SAMPLE ",sampleName,"Not in identifiers"
+				isolafil.write(str(profileST)+'\t'+str(round(meanAccur,2))+'\t'+str(sampleName)+'\n')
+				
+		isolafil.close()
+		 
 	
 print "Completed"
