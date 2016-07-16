ce227bec7342579cc415fd3b17e534e6c750ffb9
Moreno <moreno.zolfo@gmail.com>
2016-07-16 18:25:15 +0200

    added examples
    Former-commit-id: 6ec770a6216ade27900d9bbe71fd92e83bb0940e

---
+++ examples/1_single_sample/test.sh
@@ -0,0 +1,58 @@
+echo "
+-------------------------------------------------------------------------------
+          METAMLST TEST SCRIPT # 1 - ONE SAMPLE, DEFAULT DATABASE
+-------------------------------------------------------------------------------
+
+This test script executes MetaMLST on a single sample using the pre-made
+database
+-------------------------------------------------------------------------------
+
+Input     : A small subset of HMP sample SRS013261, FASTQ format
+
+Database  : Pre-made database: ../../metamlstDB_2015.db.
+
+Detection : SRS013261: S. epidermidis ST-19
+          
+Output    : - Report file:
+              - ./out/merged/sepidermidis_report.txt
+            - Updated Typing list (STs):
+              - ./out/merged/sepidermidis_ST.txt
+
+-------------------------------------------------------------------------------"
+
+#Generate a Bowtie2 index
+echo "
+-------------------------------------------------------------------------------
+Executing: metaMLST-index.py -i bowtie_MmetaMLST ../../metamlstDB_2015.db
+-------------------------------------------------------------------------------
+"
+
+../../metaMLST-index.py -i bowtie_MmetaMLST ../../metamlstDB_2015.db
+
+#Map the fastq with Bowtie
+echo "
+-------------------------------------------------------------------------------
+Executing: bowtie2 --threads 4 --very-sensitive-local -a --no-unal -x bowtie_MmetaMLST -U SRS013261_epidermidis.fastq | samtools view -bS - > SRS013261_epidermidis.bam;
+-------------------------------------------------------------------------------
+"
+
+bowtie2 --threads 4 --very-sensitive-local -a --no-unal -x bowtie_MmetaMLST -U SRS013261_epidermidis.fastq | samtools view -bS - > SRS013261_epidermidis.bam;
+
+#Run MetaMLST on a single sample
+echo "
+-------------------------------------------------------------------------------
+Executing: metaMLST.py -d sepidermidis.db SRS013261_epidermidis.bam -o ./out/
+-------------------------------------------------------------------------------
+"
+
+../../metaMLST.py -d ../../metamlstDB_2015.db SRS013261_epidermidis.bam -o ./out/
+
+#Type the STs
+echo "
+-------------------------------------------------------------------------------
+Executing: metaMLST-merge.py -d sepidermidis.db ./out
+-------------------------------------------------------------------------------
+"
+
+../../metaMLST-merge.py -d ../../metamlstDB_2015.db ./out
+
---
+++ examples/5_phylogenetic_analysis/test.sh
@@ -0,0 +1,66 @@
+echo "
+-------------------------------------------------------------------------------
+          METAMLST TEST SCRIPT # 5 - MANY SAMPLES (Phylogenetic Analysis)
+-------------------------------------------------------------------------------
+
+This test script executes MetaMLST on 27 samples, using the pre-made
+database. The script detects P. acnes and reports files suitable for 
+Phylogenetic Analysis (trees and Minimum Spanning Trees)
+-------------------------------------------------------------------------------
+
+Input     : 27 files from the HMP (filtered reads matching to P. acnes)
+
+Database  : Pre-made database: ../../metamlstDB_2015.db
+
+Detection : 13 known STs, 14 new STs (ID >100000)
+
+Output    : - Report file: ./out/merged/pacnes_report.txt
+            - ST file: ./out/merged/pacnes_ST.txt 
+            - Sequences file: ./out/merged/pacnes_sequences.txt
+
+-------------------------------------------------------------------------------"
+
+#Unzipping the archive
+unzip 5_phylogenetic_analysis_files.zip
+
+#Generate a Bowtie2 index
+echo "
+-------------------------------------------------------------------------------
+Executing: metaMLST-index.py -i bowtie_MmetaMLST ../../metamlstDB_2015.db
+-------------------------------------------------------------------------------
+"
+../../metaMLST-index.py -i bowtie_MmetaMLST ../../metamlstDB_2015.db
+
+
+for i in ./*.fastq; do
+   
+  bs=$(basename $i)
+  echo "
+-------------------------------------------------------------------------------
+Executing: bowtie2 --threads 4 --very-sensitive-local -a --no-unal -x bowtie_MmetaMLST -U "$i" | samtools view -bS - > "${bs%%.*}".bam;
+-------------------------------------------------------------------------------
+" 
+  #Map the fastq with Bowtie
+  bowtie2 --threads 4 --very-sensitive-local -a --no-unal -x bowtie_MmetaMLST -U $i | samtools view -bS - > ${bs%%.*}.bam;
+  
+  echo "
+-------------------------------------------------------------------------------
+Executing: metaMLST.py -d ../../metamlstDB_2015.db "${bs%%.*}".bam --filter pacnes -o ./out/
+-------------------------------------------------------------------------------
+" 
+  #Run MetaMLST
+  ../../metaMLST.py -d ../../metamlstDB_2015.db ${bs%%.*}.bam --filter pacnes -o ./out/
+done
+
+
+#Type the STs
+echo "
+-------------------------------------------------------------------------------
+Executing: metaMLST-merge.py -d ../../metamlstDB_2015.db --meta test_metadata.txt ./out
+-------------------------------------------------------------------------------
+"
+../../metaMLST-merge.py -d ../../metamlstDB_2015.db --meta sample_metadata.txt --outseqformat A ./out
+
+
+
+
